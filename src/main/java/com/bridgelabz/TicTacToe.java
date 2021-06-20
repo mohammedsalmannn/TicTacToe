2e790e222a4f639f26e6fadd0767247bd9402146
mohammedsalmannn <rmdsalman001@gmail.com>
2021-06-20 13:24:02 +0530

    Added : Function to Show the Current Board

--- src/main/java/com/bridgelabz/TicTacToe.java
+++ src/main/java/com/bridgelabz/TicTacToe.java
@@ -15,11 +15,22 @@ public class TicTacToe {
         return userInput.next().toUpperCase().charAt(0);
     }
 
+    public static void showBoard(char[] board) {
+        System.out.println("|-----------|");
+        System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
+        System.out.println("|-----------|");
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
         Scanner userInput = new Scanner(System.in);
         char[] board = createBoard();
         char userLetter = chooseUserLetter(userInput);
         char computerLetter = (userLetter == 'X') ? 'O' : 'X';
+        showBoard(board);
     }
 }
